fa3429868f7c4de5f97ae7283e018cb0a1ffe400
Leo Gervilla <leo.gervilla@getweave.com@Leos-MacBook-Pro-242.local>
2020-08-21 16:12:19 -0600

    added config key frome firebase

---
+++ assets/javascript/app.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    var config = {
+        apiKey: "AIzaSyBPCAxjycmuYaeY8RISjpPzUOSfmZIbQrQ",
+        authDomain: "train-scheduler-68643.firebaseapp.com",
+        databaseURL: "https://train-scheduler-68643.firebaseio.com",
+        projectId: "train-scheduler-68643",
+        storageBucket: "train-scheduler-68643.appspot.com",
+        messagingSenderId: "934455543386"
+    };
+    firebase.initializeApp(config);
